54b28db9221cb59d5a27ab62a0a9fc8a2e43e8b6
12345amal12345 <amalgkrishnan204@gmail.com>
2024-08-13 20:46:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Add from './component/Add Todo';
+import Search from './component/Search Todo';
+import View from './component/View Todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   < BrowserRouter>
+   
+   < Routes> 
+   
+   < Route path="/" element={<Add Todo/>}/>
+   < Route path="/search" element={<Search Todo/>}/>
+   < Route path="/view" element={<View Todo/>}/>
+
+   
+   </Routes>
+   
+   </BrowserRouter>
   );
 }
 
